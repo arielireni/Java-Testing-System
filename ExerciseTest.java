f9ff81b2a104e17634a5760f849556511be2ec0a
arielireni <69749882+arielireni@users.noreply.github.com>
2020-09-19 18:45:58 +0300

    Add files via upload

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -2,119 +2,172 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Scanner;
+import java.util.*;
 
-public class Tester {
-    private Scanner userInput = new Scanner(System.in);
-    private String className;
-    private String methodName;
+public class ExerciseTest {
     private Class studentClass;
-    private Method studentMethod;
-    private Class[] inputClasses;
-    private Class returnType;
-    private Object [] inputArgs;
-    private Object someInstance;
-    private int grade;
+    private List<Integer> questionsGrades = new LinkedList<>();
+    private int [] questionsScore;
+    private int totalGrade;
 
-    public Tester(Class returnType, Class [] inputClasses, Object[] inputArgs, Object someInstance) {
-        this.returnType = returnType;
-        this.inputClasses = inputClasses;
-        this.inputArgs = inputArgs;
-        this.someInstance = someInstance;
-        Scanner userInput = new Scanner(System.in);
+    public ExerciseTest(String className, int [] questionsScore) {
         // initializing student class
-        askClassNameInput();
-        ClassLoader classLoader = Tester.class.getClassLoader();
+        if(checkScore(questionsScore)) {
+            this.questionsScore = questionsScore;
+        }
+        else {
+            System.out.println("Error: invalid score entered");
+        }
+        ClassLoader classLoader = ExerciseTest.class.getClassLoader();
         try {
             this.studentClass = classLoader.loadClass(className);
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        // initializing student method
-        askMethodNameInput();
-        try {
-            this.studentMethod = studentClass.getMethod(methodName, inputClasses);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Enter expected output");
-        String expected = userInput.nextLine();
-        if(returnType == null) {
-            testForVoid(expected);
+    }
+
+    private void calcTotalGrade() {
+        if(questionsGrades.size() != questionsScore.length) {
+            System.out.println("Error: the number of questions examined does not match the number of questions requested");
         }
         else {
-            testForReturnValue(expected);
+            for(int i=0 ; i < questionsScore.length ; i++) {
+                this.totalGrade += ((double) questionsGrades.get(i) * ((double) questionsScore[i] / 100));
+            }
         }
-        System.out.println("finish testing");
-        userInput.close();
     }
 
-    private void testForVoid(String expected) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream consolePrints = new PrintStream(baos);
-        PrintStream old = System.out;
-        System.setOut(consolePrints);
-        try {
-            studentMethod.invoke(someInstance, inputArgs);
-        }
-        catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        System.setOut(old);
-        String actual = baos.toString();
-        this.grade = calcGrade(expected, actual);
+    public int getTotalGrade() {
+        calcTotalGrade();
+        return this.totalGrade;
     }
 
-    private void testForReturnValue(String expected) {
-        if(this.returnType == studentMethod.getReturnType()) { //otherwise, grade=0
-            String actual = null;
+    private boolean checkScore(int[] scores) {
+        int sum = 0;
+        for(int score: scores) {
+            sum += score;
+        }
+        return(sum == 100);
+    }
+
+    // INNER CLASS in ExerciseTest
+
+    public class QuestionTest {
+        private Method studentMethod;
+        private Class [] inputClasses;
+        private List<Integer> testsGrades = new LinkedList<>();
+        private int questionGrade;
+
+        public QuestionTest(String methodName, Class [] inputClasses) {
+            this.inputClasses = inputClasses;
+            // initializing student method
             try {
-                actual = (studentMethod.invoke(someInstance, inputArgs)).toString();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
+                this.studentMethod = studentClass.getMethod(methodName, this.inputClasses);
+            } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             }
-            this.grade = calcGrade(expected, actual);
         }
-    }
 
-    private int calcGrade(String expected, String actual) {
-        int grade = 100;
-        int length = expected.length();
-        int scorePerChar = 100/(length);
-        int max = Math.max(expected.length(), actual.length());
-        for(int i = 0 ; i < max ; i++) {
-            if(i < expected.length() && i < actual.length()) {
-                if(expected.charAt(i) != actual.charAt(i)) {
-                    grade = grade - scorePerChar;
+        private void calcQuestionGrade() {
+            int cnt = 0;
+            int result = 0;
+            for(int testGrade: testsGrades) {
+                result += testGrade;
+                cnt += 1;
+            }
+            if(cnt != 0) { // otherwise, questionGrade=0
+                this.questionGrade = result / cnt;
+            }
+            questionsGrades.add(this.questionGrade);
+        }
+
+        public int getQuestionGrade() {
+            calcQuestionGrade();
+            return this.questionGrade;
+        }
+
+
+        // INNER CLASS in QuestionTest
+
+        public class SingleTest {
+            private Class returnType;
+            private Object[] inputArgs;
+            private Object someInstance;
+            private int singleTestGrade;
+
+            public SingleTest(Class returnType, Object[] inputArgs, Object someInstance, String expected, boolean moreTests) {
+                this.returnType = returnType;
+                this.inputArgs = inputArgs;
+                this.someInstance = someInstance;
+                if(returnType == null) {
+                    testForVoid(expected);
+                }
+                else {
+                    testForReturnValue(expected);
+                }
+                testsGrades.add(this.singleTestGrade);
+                if(! moreTests) { // finished with all the tests for this question, ready to calculate total grade for this question
+                    calcQuestionGrade();
                 }
             }
-            else {
-                grade = grade - scorePerChar;
+
+            private void testForVoid(String expected) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                PrintStream consolePrints = new PrintStream(baos);
+                PrintStream old = System.out;
+                System.setOut(consolePrints);
+                try {
+                    studentMethod.invoke(someInstance, inputArgs);
+                }
+                catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+                System.setOut(old);
+                String actual = baos.toString();
+                //System.out.println(expected + " " + actual);
+                this.singleTestGrade = calcSingleTestGrade(expected, actual);
+            }
+
+            private void testForReturnValue(String expected) {
+                if(this.returnType == studentMethod.getReturnType()) { //otherwise, grade=0
+                    String actual = null;
+                    try {
+                        actual = (studentMethod.invoke(someInstance, inputArgs)).toString();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                    this.singleTestGrade = calcSingleTestGrade(expected, actual);
+                }
+            }
+
+            private int calcSingleTestGrade(String expected, String actual) {
+                double correct = 0;
+                double wrong = 0;
+                int grade;
+                if (expected.length() != actual.length()) {
+                    wrong += Math.abs(expected.length() - actual.length());
+                }
+                for(int i=0 ; i < Math.min(expected.length(), actual.length()) ; i++) {
+                    if(expected.charAt(i) == actual.charAt(i)) {
+                        correct += 1;
+                    }
+                    else {
+                        wrong += 1;
+                    }
+                }
+                grade = (int) ((correct / (correct + wrong)) * 100);
+                return grade;
+            }
+
+            public int getSingleTestGrade() {
+                return this.singleTestGrade;
             }
         }
-        if(grade < 0) {
-            grade = 0;
-        }
-        return grade;
-    }
-
-    public int getGrade() {
-        return this.grade;
-    }
-
-    private void askClassNameInput() {
-        System.out.println("Enter class name");
-        this.className = userInput.nextLine();
-    }
-
-    private void askMethodNameInput() {
-        System.out.println("Enter method name");
-        this.methodName = userInput.nextLine();
     }
 }
