9616fe5ea7dded0175ae172e8b060c1a44bebc4b
arielireni <69749882+arielireni@users.noreply.github.com>
2020-09-30 18:07:23 +0300

    Add files via upload

--- DataAnalysis-CM.py
+++ DataAnalysis-CM.py
@@ -8,13 +8,13 @@ workbook = xlrd.open_workbook("C:\\Users\\Eli\\Desktop\\testGrades\\dataCollecto
 sheet = workbook.sheet_by_index(1)
 
 title_text = 'Common Mistakes'
-fig_background_color = 'firebrick'
-fig_border = 'lightcoral'
+fig_background_color = 'lightcyan'
+fig_border = 'lightskyblue'
 data = [['Question Number', 'Test Number', 'Expected Value', 'Average Grade']]
 for i in range(sheet.nrows):
     data.append([str(sheet.cell_value(i, j)) for j in range(0, 4)])
 column_headers = data.pop(0)
-ccolors = plt.cm.Reds(np.full(len(column_headers), 0.1))
+ccolors = plt.cm.Blues(np.full(len(column_headers), 0.1))
 
 plt.figure(linewidth=2, edgecolor=fig_border, facecolor=fig_background_color, tight_layout={'pad':1},)
 the_table = plt.table(cellText=data, rowLoc='right', colColours=ccolors, colLabels=column_headers, loc='center')
@@ -26,7 +26,7 @@ ax = plt.gca()
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 plt.box(on=None)
-plt.suptitle(title_text, size=20, color='w')
+plt.suptitle(title_text, size=20)
 plt.draw()
 fig = plt.gcf()
 plt.show()
